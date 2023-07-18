fbbdb9d76582fcfc1fcdc710b8bb5a6de73bfba9
Sarthak Pan <sarthakpan@gmail.com>
2023-07-18 13:05:09 +0530

    Added my project

---
+++ copilotservice/src/main/java/com/copilot/copilotservice/CopilotserviceApplication.java
@@ -0,0 +1,13 @@
+package com.copilot.copilotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotserviceApplication.class, args);
+	}
+
+}
---
+++ copilotservice/src/test/java/com/copilot/copilotservice/CopilotserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copilot.copilotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

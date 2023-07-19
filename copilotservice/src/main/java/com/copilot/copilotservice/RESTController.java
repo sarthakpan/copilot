ebdc8c498f1dddba7ed13c4dcb2c0b097ddc3c78
Sarthak Pan <sarthakpan@gmail.com>
2023-07-19 16:23:52 +0530

    updates

--- copilotservice/src/main/java/com/copilot/copilotservice/RESTController.java
+++ copilotservice/src/main/java/com/copilot/copilotservice/RESTController.java
@@ -1,7 +1,8 @@
 package com.copilot.copilotservice;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 // generate a rest controller at path of /
 @RestController("/")
@@ -19,6 +20,39 @@ public class RESTController {
     public String register(String username, String password, String confirmPassword, String address, String city, String country, String zip) {
         //return a string of the username and password
         return "Username: " + username + " Password: " + password + " Confirm Password: " + confirmPassword + " Address: " + address + " City: " + city + " Country: " + country + " Zip: " + zip;
+    }
+
+    //generate a post method to register with username,password, confirmPassword, address, city, country, zip as Request body
+    @PostMapping("/register2")
+    public String register2(@RequestBody User user) {
+        //return a string of the username and password
+        return "Username: " + user.getUsername() + " Password: " + user.getPassword() + " Confirm Password: " + user.getConfirmPassword() + " Address: " + user.getAddress() + " City: " + user.getCity() + " Country: " + user.getCountry() + " Zip: " + user.getZip();
+    }
+
+    //generate a post method to register with username,password, confirmPassword, address, city, country, zip as Request body
+    @PostMapping("/register3")
+    public String register3(@RequestBody User user) {
+        //return a string of the username and password
+        return "Username: " + user.getUsername() + " Password: " + user.getPassword() + " Confirm Password: " + user.getConfirmPassword() + " Address: " + user.getAddress() + " City: " + user.getCity() + " Country: " + user.getCountry() + " Zip: " + user.getZip();
+    }
+
+    //generate a get method with path varaible of id
+    @GetMapping("/user/{id}")
+    public String getUser(@PathVariable String id) {
+        //return a string of the id
+        return "ID: " + id;
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login2(String username, String password) {
+        //return a string of the username and password
+        return new ResponseEntity<>("result successful result",
+                HttpStatus.OK);
+    }
+    @PostMapping("/register")
+    public ResponseEntity<String> register2(String username, String password, String confirmPassword, String address, String city, String country, String zip) {
+        //return a string of the username and password
+        return new ResponseEntity<>("result successful result",
+                HttpStatus.OK);
     }
 
 }
--- copilotservice/src/main/java/com/copilot/copilotservice/User.java
+++ copilotservice/src/main/java/com/copilot/copilotservice/User.java
@@ -1,2 +1,20 @@
-package com.copilot.copilotservice;public class User {
+package com.copilot.copilotservice;
+
+// import lambok getters and setters
+import lombok.Getter;
+import lombok.Setter;
+
+// annotation for getters and setters
+@Getter
+@Setter
+public class User {
+    // user class
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String address;
+    private String city;
+    private String country;
+    private String zip;
+
 }
